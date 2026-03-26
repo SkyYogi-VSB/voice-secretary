0a8bfb46467d19d0146e47daf5b58f0337a063a1
SkyBikeYogi <sagarbaliyara@gmail.com>
2026-03-25 22:04:43 -0700

    minor changes

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-// ── Private config — do NOT commit this file ──
 const CONFIG = {
   GOOGLE_CLIENT_ID: '584266523790-8lp1mdvli051oecou6kq6vgbpeqi9a7r.apps.googleusercontent.com',
   WORK_FOLDER_NAME: 'cos-notes-work',

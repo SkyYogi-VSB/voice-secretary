15b370da084722529a19ac5fdc6071186c0208d3
SkyBikeYogi <sagarbaliyara@gmail.com>
2026-03-25 22:01:36 -0700

    fixed client

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // ── Private config — do NOT commit this file ──
 const CONFIG = {
-  GOOGLE_CLIENT_ID: '584266523790-',
+  GOOGLE_CLIENT_ID: '584266523790-8lp1mdvli051oecou6kq6vgbpeqi9a7r.apps.googleusercontent.com',
   WORK_FOLDER_NAME: 'cos-notes-work',
   PERSONAL_FOLDER_NAME: 'cos-notes-personal'
 };
